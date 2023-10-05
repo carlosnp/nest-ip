aae4028f3b67defe08b7b0eed1be6dd54c41d17c
Carlos Nava <machinery.e@gmail.com>
2023-10-05 15:16:00 +0000

    Prueba con la libreria nestjs-real-ip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RealIP } from 'nestjs-real-ip';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello() {
-    return this.appService.getHello();
+  async getHello(@RealIP() ip: string) {
+    const data = await this.appService.getHello();
+    return { ...data, myIp: ip };
   }
 }
